8d3f29664884423198dbcc0edea53d5f3c12d0c6
Stephen A. Daire <73073621+Proteanindustries@users.noreply.github.com>
2022-07-07 21:11:00 -0400

    Update age_license_check.py

--- age_license_check.py
+++ age_license_check.py
@@ -1,13 +1,20 @@
-is_of_age = True
-is_licensed = True
+def of_age():
+    is_of_age = int(age) >18
 
-if is_of_age == True and is_licensed ==True :
-    print('You are old enough and are licensed to drive!')
-elif is_of_age == False and is_licensed == False:
-    print('You are not old enough or not licensed to drive!')
-elif is_of_age == True and is_licensed == False:
-    print('You are not licensed to drive!')
-else:
-    print('Find an older, licensed driver.')
+def is_licensed():
+    is_licensed == True
 
-print('Safety First, Enjoy Your Ride!')
+def drivercheck(is_of_age = 0, is_licensed = False):
+    if is_of_age > 18 and is_licensed == True:
+        print('You are old enough and are licensed to drive!')
+    elif is_of_age < 18 and is_licensed == False:
+        print('You are not old enough or not licensed to drive!')
+    elif is_of_age > 18 and is_licensed == False:
+        print('You are not licensed to drive!')
+    else:
+        print('Find an older, licensed driver.')
+    print('Safety First!')
+
+drivercheck(19,True)
+drivercheck(25, False)
+drivercheck(15, False)
